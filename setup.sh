a0ce7846f580592a696447ae6151ee7253aba319
Daniel Weibel <danielmweibel@gmail.com>
2023-08-04 05:54:38 +0200

    Add loop for selecting optional tools to install
    Also add installation functions for a few of the tools.

--- setup.sh
+++ setup.sh
@@ -48,14 +48,17 @@ msg() { __print "\n==> $1\n" cyan; }
 ack() { __print "    OK\n" green; }
 msg-sub() { __print "    - $1"; }
 ack-sub() { __print "OK\n" green; }
+msg-sub-sub() { __print "        $1"; }
 msg-bare() { __print "$1" "$2"; }
 err() { __print "ERROR\n$1\n" red; exit 1; }
 is-installed() { which "$1" &>/dev/null; }
 is-root() { [[ "$UID" = 0 ]]; }
 has-cached-sudo-password() { sudo -n true 2>/dev/null; }
 run() { eval "$*" &>>"$LOG" || err "Command \"$*\" failed: see $LOG for details"; }
+run-pipe() { eval "$*" | tee -a "$LOG" || err "Command \"$*\" failed: see $LOG for details"; }
 run-root() { is-root && run "$*" || run "sudo $*"; }
 run-apt() { run-root apt -o Acquire::http::Timeout=5 -o APT::Update::Error-Mode=any -o APT::Get::Assume-Yes=true "$@"; }
+get-input() { read in; __print "$in\n"; }
 
 #------------------------------------------------------------------------------#
 # Banner
@@ -127,38 +130,39 @@ ack-sub
 # Install packages
 #------------------------------------------------------------------------------#
 
-packages=(
-curl
-dos2unix
-fzf
-gettext
-gifsicle
-git
-imagemagick
-iproute2
-iputils-ping
-jq
-lsb-release
-neovim
-net-tools
-netcat
-python-is-python3
-python3
-python3-pip
-ripgrep
-traceroute
-tree
-unzip
-vim
-wget
-)
-
 msg "Installing packages..."
+
+packages=(
+  curl
+  dos2unix
+  fzf
+  gettext
+  gifsicle
+  git
+  imagemagick
+  iproute2
+  iputils-ping
+  jq
+  lsb-release
+  neovim
+  net-tools
+  netcat
+  python-is-python3
+  python3
+  python3-pip
+  ripgrep
+  traceroute
+  tree
+  unzip
+  vim
+  wget
+)
 
 msg-sub "Updating package lists: "
 run-apt update
 ack-sub
 for p in "${packages[@]}"; do
+  # TODO: check if package is already installed, similar to the optional tools
   msg-sub "Installing '$p': "
   run-apt install "$p"
   ack-sub
@@ -204,19 +208,177 @@ fi
 ack-sub
 
 #------------------------------------------------------------------------------#
-# Install optional specialised tools
+# Optional tools
 #------------------------------------------------------------------------------#
 
-# TODO: start loop asking whether to install each tool
-# Tools:
-# - Go
-# - Terraform
-# - Docker
-# - kubectl
-# - AWS CLI
-# - Azure CLI
-# - Google Cloud CLI
-# - Grip
+tools=(
+  Grip
+  Go
+  Terraform
+  Docker
+  kubectl
+  "AWS CLI"
+  "Azure CLI"
+  "Google Cloud CLI"
+)
+
+is-tool-installed() {
+  case "$1" in
+    Grip) is-installed grip ;;
+    Go) is-installed go ;;
+    Terraform) is-installed terraform ;;
+    Docker) is-installed docker ;;
+    kubectl) is-installed kubectl ;;
+    "AWS CLI") is-installed aws ;;
+    "Azure CLI") is-installed az ;;
+    "Google Cloud CLI") is-installed gcloud ;;
+    *) err "Unknown tool: $1"
+  esac
+}
+
+# Convert a tool name to a corresponding installation function name
+function-name() {
+  echo "install-$(echo "$1" | tr '[:upper:]' '[:lower:]' | sed 's/[^a-zA-Z0-9]/-/g')"
+}
+
+__get-distro-codename() { lsb_release -cs; }
+__get-arch() { dpkg --print-architecture; }
+__add-debian-repo() {
+  key_url=$1
+  key_file=$2
+  repo_url=$3
+  repo_file=$4
+  repo_distro=$5
+  repo_comp1=$6
+  run-pipe curl -fsSL "$key_url" | run-root gpg --yes --dearmor -o "$key_file"
+  run-pipe echo "deb [arch=$(__get-arch) signed-by=$key_file] $repo_url $repo_distro $repo_comp1" | run-root tee "$repo_file"
+}
+
+install-grip() {
+  run "pip install grip"
+}
+
+install-go() {
+  run-apt "install golang"
+}
+
+install-terraform() {
+  __add-debian-repo \
+      https://apt.releases.hashicorp.com/gpg \
+      /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+      https://apt.releases.hashicorp.com \
+      /etc/apt/sources.list.d/hashicorp.list \
+      $(__get-distro-codename) \
+      main
+  run-apt update
+  run-apt install terraform
+}
+
+install-docker() {
+  __add-debian-repo \
+      https://download.docker.com/linux/ubuntu/gpg \
+      /etc/apt/keyrings/docker.gpg \
+      https://download.docker.com/linux/ubuntu \
+      /etc/apt/sources.list.d/docker.list \
+      $(__get-distro-codename) \
+      stable
+  run-apt update
+  run-apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  if ! is-root; then
+    run-root usermod -aG docker "$USER"
+    newgrp docker
+  fi
+}
+
+install-kubectl() {
+#  curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+#
+#  echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+#
+#  # Install the kubectl package
+#  run-apt update
+#  run-apt install kubectl
+#
+#  # Install command completion
+#  # https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/#enable-kubectl-autocompletion
+#  kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl >/dev/null
+}
+
+install-azure-cli() {
+#  curl -sLS https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/keyrings/microsoft.gpg >/dev/null
+#  #sudo chmod go+r /etc/apt/keyrings/microsoft.gpg
+#
+#  # Add the Azure CLI Debian repository
+#  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/azure-cli/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+#
+#  # Install the Azure CLI package
+#  run-apt update
+#  run-apt install azure-cli
+}
+
+#------------------------------------------------------------------------------#
+# Select optional tools to install
+#------------------------------------------------------------------------------#
+
+msg "Select tools to install..."
+tool-selection-dialog() {
+
+  # Tools selected for installation
+  tools_install=()
+
+  # Read answers from user
+  msg-sub "Select tools: [y]es (default), [n]o, [s]top\n"
+  for t in "${tools[@]}"; do
+    msg-sub "$t: "
+    case "$(get-input)" in
+      y*|Y*|"") tools_install+=("$t") ;;
+      s*|S*) break ;;
+      n*|N*|*) ;;
+    esac
+  done
+
+  # Review selected tools
+  msg-sub "Selected tools:"
+  if [[ "${#tools_install[@]}" -gt 0 ]]; then
+    msg-bare "\n"
+    ((i=1))
+    for t in "${tools_install[@]}"; do
+      msg-sub-sub "$i. $t\n"
+      ((i++))
+    done
+  else
+    msg-bare " <none>\n"
+  fi
+
+  # Proceed to installation or repeat selection
+  msg-sub "Proceed to installation? [y]es (default), [n]o: "
+  case "$(get-input)" in
+    y*|Y*|"") ;;
+    n*|N*|*) tool-selection-dialog ;;
+  esac
+}
+
+tool-selection-dialog
+
+#------------------------------------------------------------------------------#
+# Install selected optional tools
+#------------------------------------------------------------------------------#
+
+msg "Installing selected tools..."
+
+for t in "${tools_install[@]}"; do
+  if ! is-tool-installed "$t"; then
+    msg-sub "Installing $t: "
+    eval "$(function-name "$t")"
+  else
+    msg-sub "$t is already installed: "
+  fi
+  ack-sub
+done
+
+#------------------------------------------------------------------------------#
+# End
+#------------------------------------------------------------------------------#
 
 msg "Done!"
 msg-sub "See logs in $LOG\n"
